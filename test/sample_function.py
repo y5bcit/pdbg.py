0c93de689c144262532e7d965429807d1fa5c73a
y5bcit <yruan5@my.bcit.ca>
2019-04-11 09:44:23 -0700

    fix indentation display error

--- test/sample_function.py
+++ test/sample_function.py
@@ -1,12 +1,12 @@
 def function_one(a):
-	for i in range(3):
-		a += 1
-	return a
+    for i in range(3):
+        a += 1
+    return a
 
 def function_two(b):
-	for i in range(3):
-		b += 1
-	return b
+    for i in range(3):
+        b += 1
+    return b
 
 c = function_one(1)
 d = function_two(2)
